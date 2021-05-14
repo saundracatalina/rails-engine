631b181e21a0ce137f02e5a883d770470315f066
Saundra Catalina <saundracatalina@gmail.com>
2021-05-13 19:06:22 -0600

    Add sad path test for a patch with an ID that does not exist

--- spec/requests/api/v1/merchants/update_a_merch_record_spec.rb
+++ spec/requests/api/v1/merchants/update_a_merch_record_spec.rb
@@ -19,4 +19,16 @@ describe 'update a merchant record' do
       expect(merchants[:attributes][:name]).to_not eq("Bob")
     end
   end
+  describe 'sad path' do
+    it 'throws a 404 when an id that does not exist is entered for a patch' do
+      merchant = Merchant.create!(name: "Bob")
+      fake_merchant_id = 9999999
+      merchant_headers = {'CONTENT_TYPE' => 'application/json'}
+      merchant_params = {name: "Steve"}
+
+      patch "/api/v1/merchants/#{fake_merchant_id}", headers: merchant_headers, params: JSON.generate(merchant_params)
+
+      expect(response.status).to eq(404)
+    end
+  end
 end

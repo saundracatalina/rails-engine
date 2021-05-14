26f76d5c5b2404fce3ae17895f1cfc71ad99db84
Saundra Catalina <saundracatalina@gmail.com>
2021-05-13 18:55:13 -0600

    Add tests to validate attributes required to create an Item record

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,9 +4,9 @@ describe Item do
   describe 'relationships' do
     it {should belong_to :merchant}
   end
-  # describe 'validations' do
-  #   it {validate_presence_of :name}
-  #   it {validate_presence_of :description}
-  #   it {validate_presence_of :unit_price}
-  # end
+  describe 'validations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :description}
+    it {should validate_presence_of :unit_price}
+  end
 end
